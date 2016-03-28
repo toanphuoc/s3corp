b89a1e4e81fe0fb00e0606eaddd0bf2274a3449c
phuoctoanit <toanhcmus@gmail.com>
2016-03-28 23:08:03 +0700

    h

---
+++ src/main/java/com/s3corp/web/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.s3corp.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	@RequestMapping(method = RequestMethod.GET)
+    public String user() {
+        return "users";
+    }
+ 
+    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+        return "welcome";
+    }
+}

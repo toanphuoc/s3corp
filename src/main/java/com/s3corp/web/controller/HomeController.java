f65b4077b555ccc4c2dfc5d0eb9150513ded9a84
phuoctoanit <toanhcmus@gmail.com>
2016-03-29 21:31:51 +0700

    add new template

--- src/main/java/com/s3corp/web/controller/HomeController.java
+++ src/main/java/com/s3corp/web/controller/HomeController.java
@@ -7,17 +7,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
-public class HelloWordController {
+public class HomeController {
 
 	@RequestMapping(method = RequestMethod.GET)
-    public String sayHello() {
+    public String getIndexPage() {
       //  model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-        return "users";
-    }
- 
-    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
-    public String sayHelloAgain(ModelMap model) {
-        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
-        return "welcome";
+        return "index";
     }
 }

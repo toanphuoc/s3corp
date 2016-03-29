3706e374e503238fb0dd3a4191c55a2dca760903
phuoctoanit <toanhcmus@gmail.com>
2016-03-30 01:02:52 +0700

    lll

--- src/main/webapp/static/js/service/user_service.js
+++ src/main/webapp/static/js/service/user_service.js
@@ -4,7 +4,7 @@ var appServices = angular.module('appServices', ['ngResource']);
 appServices.factory('UserService', ['$http', function($http){
 
 	var factory = {};
-	factory.	 = function(){
+	factory.fetchAllUsers = function(){
 		return $http({
 			url: 'http://localhost:8080/web/user/getList'
 		});
